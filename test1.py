9a176cf9ea42f75000c5fbbe646717cac5ddac09
yRxf <39730396+yRxf@users.noreply.github.com>
2020-06-04 11:07:50 +0800

    不定参数函数

---
+++ test1.py
@@ -0,0 +1,11 @@
+def test1(*args,**dics):
+    if args:
+        for arg in args:
+            print(arg)
+    else:
+        print("没有传递参数！")
+    if dics:
+        for key,value in dics.items():
+            print(key+":"+value)
+    else:
+        print("没有传递字典！")
